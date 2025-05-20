9b723cce9e2fec248ca79738f9a2da750568b68a
Minji Kil <yellowminji@naver.com>
2025-05-20 11:23:30 +0900

    [ADD] Assets

---
+++ Assets/02. Scripts/Movement.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float moveSpeed;
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            this.transform.position = this.transform.position + Vector3.forward * (moveSpeed * Time.deltaTime);    
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            this.transform.position = this.transform.position + Vector3.back * (moveSpeed * Time.deltaTime);    
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            this.transform.position = this.transform.position + Vector3.left * (moveSpeed * Time.deltaTime);    
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            this.transform.position = this.transform.position + Vector3.right * (moveSpeed * Time.deltaTime);    
+        }
+        
+    }
+}
